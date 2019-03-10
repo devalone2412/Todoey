37c8a2e9e332843c53425c3019a54123dce267b7
Luc Thoi Sang <devbrownbear@gmail.com>
2019-03-10 14:07:08 +0700

    Add Data Model folder to project

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Luc Thoi Sang on 3/10/19.
+//  Copyright © 2019 Luc Thoi Sang. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
